d2c0d4a568a855614e56d5d0841d71be723f75d4
77 <a4860e@gmail.com>
2020-11-20 14:33:23 +0800

    create repo add submodule add strategy add .gitignore

---
+++ src/strategy/include/strategy/WeightLiftingInfo.h
@@ -0,0 +1,86 @@
+#pragma once
+#ifndef WEIGHTLIFTINGINFO_H
+#define WEIGHTLIFTINGINFO_H
+
+//=========================================================================== Shengru
+#define InitialHeadX 511
+#define InitialHeadY 350
+
+enum ACTION_STATE{
+
+	headdown,PickUp,Check,Near,tweak,FirstLifting,SecondLifting,checkmidline,WalkingOverTape
+};
+//===========================================================================
+
+class WeightLiftingInfo
+{
+	public:
+		int BodyState;
+		int HeadPostitionX;
+		int HeadPostitionY;
+		float imu_initial;
+		bool DIO_fake;
+		int etLine;
+		int LineLabel2;//red
+		int LineLabel;
+//===========================================================================
+		int Firstlifting_sector;
+		int Firstlifting_sup_sector;
+		int Secondlifting_sector;
+		int Secondlifting_sup_sector;
+		int continuous_speed;
+		int continuous_Y;
+		int continuous_theta;
+		int continuous_imu;
+		int continuous_stopdistance;
+		int tweak_stopdistance;
+		int continuous_second_speed;
+		int continuous_second_Y;
+		int continuous_second_theta;
+		int continuous_second_imu;
+		int continuous_third_speed;
+		int continuous_third_Y;
+		int continuous_third_theta;
+		int continuous_third_imu;
+		int Near_left;
+		int tweak_range;
+		int turn_left_X; 
+		int turn_left_Y; 
+		int turn_left_tweak; 
+		int turn_right_X; 
+		int turn_right_Y; 
+		int turn_right_tweak;
+		int turn_tweak_imu;
+		int tweak_stra_step;
+		int tweak_stra_theta;
+		int left_tweak_x_large;
+		int left_tweak_y_large;
+		int left_tweak_theta_large;
+		int left_tweak_large_imu;
+		int right_tweak_x_large;
+		int right_tweak_y_large;
+		int right_tweak_theta_large;
+		int right_tweak_large_imu;
+		int tweak_delay;
+		int FirstLifting_motion_delay;
+		int SecondLifting_delay;
+		int speed;
+		int first_speed;
+		int speed_up;
+//===========================================================================
+		int point_pos[4][4]={{0}};
+		
+		int count;
+		double time_start;
+		double time_end;
+
+		bool stand_flag=false;
+		bool finallookline_flag;
+		bool closeimage;
+
+		WeightLiftingInfo(void);
+};
+
+extern WeightLiftingInfo* weightlifting_info;
+#endif
+
